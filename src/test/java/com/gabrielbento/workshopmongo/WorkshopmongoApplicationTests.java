02620fe5ceeafd6a81763455813de64db4cbf6b9
Gabriel Bento <gabrielbgwork@gmail.com>
2024-09-29 22:47:19 -0300

    first commit

---
+++ src/test/java/com/gabrielbento/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielbento.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
