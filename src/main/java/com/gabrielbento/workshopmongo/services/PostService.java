9eca0871cffcb0e9620a6847365fb39064a5ab57
Gabriel Bento <gabrielbgwork@gmail.com>
2024-09-30 14:34:44 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gabrielbento/workshopmongo/services/PostService.java
@@ -0,0 +1,32 @@
+package com.gabrielbento.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielbento.workshopmongo.domain.Post;
+import com.gabrielbento.workshopmongo.domain.User;
+import com.gabrielbento.workshopmongo.dto.UserDTO;
+import com.gabrielbento.workshopmongo.repository.PostRepository;
+import com.gabrielbento.workshopmongo.repository.UserRepository;
+import com.gabrielbento.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public List<Post> findAll(){
+		return repository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		
+		return post.orElseThrow(()-> new ObjectNotFoundException("Post não encontrado!"));
+	}
+	
+}
