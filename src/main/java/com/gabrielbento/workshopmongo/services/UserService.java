4129791c5d05004e25bc9a36367fbd4c5099c418
Gabriel Bento <gabrielbgwork@gmail.com>
2024-09-30 08:37:57 -0300

    Obtendo um usuário por id

--- src/main/java/com/gabrielbento/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielbento/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gabrielbento.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabrielbento.workshopmongo.domain.User;
 import com.gabrielbento.workshopmongo.repository.UserRepository;
+import com.gabrielbento.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		
+		return user.orElseThrow(()-> new ObjectNotFoundException("Usuário não encontrado!"));
 	}
 }
